314e21e6a2e8edc5c2657f5012193bcf03f316aa
fazal wahab <wahabdir567@gmail.com>
2025-02-20 05:27:04 -0800

    add new

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,7 @@
-module.exports = {
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
-          },
-        ],
-      },
-    ];
-  },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
+
+export default nextConfig;
